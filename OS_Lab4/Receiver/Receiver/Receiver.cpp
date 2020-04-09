423781fdb1849d758603b813305d8db8b7935330
Nikita <nikita.malewich@gmail.com>
2020-04-09 19:55:13 +0300

    Lab4 done

---
+++ OS_Lab4/Receiver/Receiver/Receiver.cpp
@@ -0,0 +1,98 @@
+﻿// Receiver.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    wchar_t filename[20];
+    int num_records;
+    int num_processes;
+
+    cout << "Enter the name of the binary file: ";
+    wscanf_s(L"%ls", filename, (unsigned)_countof(filename));
+    cout << "Enter number of records: ";
+    cin >> num_records;
+    cout << "Enter number of processes: ";
+    cin >> num_processes;
+
+    ofstream out_file(filename, ios::binary);
+
+    wchar_t sender_com_line[100];
+    wchar_t event_name[30];
+    STARTUPINFO* senders_si = new STARTUPINFO[num_processes];
+    PROCESS_INFORMATION* senders_pi = new PROCESS_INFORMATION[num_processes];
+    HANDLE h_mutex = CreateMutex(NULL, FALSE, L"sender_mutex");
+    HANDLE* h_start_events = new HANDLE[num_processes];
+    HANDLE h_record_semaphore = CreateSemaphore(NULL, 0, num_records, L"sender_record_semaphore");
+    HANDLE h_empty_semaphore = CreateSemaphore(NULL, num_records, num_records, L"sender_empty_semaphore");
+
+    WaitForSingleObject(h_mutex, INFINITE);
+
+    for (int i = 0; i < num_processes; i++) {
+        ZeroMemory(&senders_si[i], sizeof(STARTUPINFO));
+        senders_si[i].cb = sizeof(STARTUPINFO);
+        wsprintf(sender_com_line, L"Sender.exe %ls %d", filename, i);
+        wsprintf(event_name, L"sender_start_event%d", i);
+
+        h_start_events[i] = CreateEvent(NULL, FALSE, FALSE, event_name);
+        CreateProcess(NULL, sender_com_line, NULL, NULL, TRUE, CREATE_NEW_CONSOLE, NULL, NULL, &senders_si[i], &senders_pi[i]);
+    }
+
+    WaitForMultipleObjects(num_processes, h_start_events, TRUE, INFINITE);
+    ReleaseMutex(h_mutex);
+
+    char command;
+    char message[20];
+    ifstream in_file(filename, ios::binary);
+    
+    while (true) {
+        cout << "Do you want to continue working? Enter y(yes) or n(no): ";
+        cin >> command;
+
+        if (command == 'y') {
+            WaitForSingleObject(h_record_semaphore, INFINITE);
+            WaitForSingleObject(h_mutex, INFINITE);
+
+            in_file.read(reinterpret_cast<char*>(message), sizeof(char) * 20);
+            cout << message << endl;
+
+            ReleaseMutex(h_mutex);
+            ReleaseSemaphore(h_empty_semaphore, 1, NULL);
+        }
+        else
+            break;
+
+    }
+
+    for (int i = 0; i < num_processes; i++) {
+        CloseHandle(senders_pi[i].hThread);
+        CloseHandle(senders_pi[i].hProcess);
+        CloseHandle(h_start_events[i]);
+    }
+    CloseHandle(h_mutex);
+    CloseHandle(h_record_semaphore);
+    CloseHandle(h_empty_semaphore);
+    delete[] senders_si;
+    delete[] senders_pi;
+    delete[] h_start_events;
+    in_file.close();
+    out_file.close();
+    
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ OS_Lab4/Sender/Sender/Sender.cpp
@@ -0,0 +1,60 @@
+﻿// Sender.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <Windows.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    char* filename = argv[1];
+    int num_process = atoi(argv[2]);
+    wchar_t event_name[30];
+
+    wsprintf(event_name, L"sender_start_event%d", num_process);
+
+    HANDLE h_record_semaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"sender_record_semaphore");
+    HANDLE h_empty_semaphore = OpenSemaphore(SEMAPHORE_ALL_ACCESS, FALSE, L"sender_empty_semaphore");
+    HANDLE h_mutex = OpenMutex(SYNCHRONIZE, FALSE, L"sender_mutex");
+    HANDLE h_start_event = OpenEvent(EVENT_MODIFY_STATE, FALSE, event_name);
+
+    SetEvent(h_start_event);
+
+    ofstream out_file(filename, ios::binary);
+    char command;
+    char message[20] = "zxcvbnmasdfghjklqwe";
+
+    while (true) {
+        cout << "Do you want to continue working? Enter y(yes) or n(no): ";
+        cin >> command;
+
+        if (command == 'y') {
+            WaitForSingleObject(h_empty_semaphore, INFINITE);
+            WaitForSingleObject(h_mutex, INFINITE);
+
+            out_file.write(reinterpret_cast<char*>(message), sizeof(char)* 20);
+
+            ReleaseMutex(h_mutex);
+            ReleaseSemaphore(h_record_semaphore, 1, NULL);
+        }
+        else
+            break;
+    }
+
+    out_file.close();
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
